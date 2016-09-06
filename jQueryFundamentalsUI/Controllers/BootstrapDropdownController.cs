2453d4a3e92059e0555b6cb1bcdd7a4dc503bc27
hemantshelar <hemant.shelar@gmail.com>
2016-09-07 09:15:29 +1000

    Added bootstrap example option.

---
+++ jQueryFundamentalsUI/Controllers/BootstrapDropdownController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace jQueryFundamentalsUI.Controllers
+{
+    public class BootstrapDropdownController : Controller
+    {
+        // GET: BootstrapDropdown
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
